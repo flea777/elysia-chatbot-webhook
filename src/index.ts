4f239ce4bc19f11e232a13f9014c0944988fe5a8
Pedro Bim <pedrodinizz2@outlook.com>
2025-07-27 23:11:00 -0300

    Webhook finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().post('/webhook', async ({ body }) => {
+  console.log('Mensagem recebida!')
+  console.log(JSON.stringify(body, null, 2))
+  return { status: 'ok' }
+})
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.listen({
+  hostname: '0.0.0.0',
+  port: 3000,
+})
